90c1aaeba30cc9eea7ae4d5e4ed4566d7388d9d5
daytch <van.daytch@gmail.com>
2023-02-27 14:35:46 +0700

    fixing

--- src/redux/middleware/api.js
+++ src/redux/middleware/api.js
@@ -6,11 +6,15 @@ var token = localStorage.getItem("token");
 let api = axios.create({
   baseURL: API_URL,
   // timeout: 10000,
-  // headers: {
-  //   "Access-Control-Allow-Origin": "*",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     // "Content-Type": "application/json;charset=utf-8",
     //   "x-access-token": token ? token : "",
-  // },
+  },
 });
 
 api.interceptors.request.use(
